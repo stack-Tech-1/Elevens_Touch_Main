57bc2e68126d389a6686328d525bc6a7e6704ed1
stack-Tech-1 <stacktechnologies0@gmail.com>
2025-05-31 10:01:52 +0100

    moved the backend

--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllProducts, getProductById } = require('../controllers/productController');
-
-router.get('/', getAllProducts);
-router.get('/:id', getProductById);
-
-module.exports = router;
-// This code sets up routes for fetching all products and a specific product by ID in a Node.js application using Express.js.
